d8ff9be4a3f5be68e82ac97b6eede22e038f2e51
Sergey-Andr <127496804+Sergey-Andr@users.noreply.github.com>
2024-06-17 14:43:59 +0300

    fixes

--- src/service/fetchCakeById.ts
+++ src/service/fetchCakeById.ts
@@ -2,7 +2,7 @@ export async function fetchCakeById(id: number) {
   const data = await fetch(`${process.env.NEXT_API_URL}/api/cake/${id}`, {
     method: "GET",
   });
-  console.log(data);
+
   const cake = await data.json();
 
   return { data: cake };
